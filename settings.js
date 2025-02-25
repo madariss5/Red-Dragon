f960505d55df568128e2bb120fb7ec494b2b6a02
madariss5 <menyhartmartin4@gmail.com>
2025-02-25 16:51:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['+4915563151347']
+global.ownertag = '4915563151347' //your tag number
+global.botname = '𝔹𝕃𝔸ℂ𝕂𝕊𝕂𝕐-𝕄𝔻' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+4915563151347'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
